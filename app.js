3d2a051e25ddc90bac0e535abfb341fc078ad78d
Lilian <210286008+liliann19@users.noreply.github.com>
2025-11-02 12:10:56 -0800

    created confirmation page and define routes

--- app.js
+++ app.js
@@ -4,10 +4,41 @@ const app = express();
 
 const PORT = 3002;
 
+const submissions = []
+
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+});
+
+app.get('/admin', (req, res) => {
+    res.send(submissions);
+})
+
+app.post('/submit-request', (req, res) => {
+    const submission = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        title: req.body.title,
+        company: req.body.company,
+        url: req.body.url,
+        email: req.body.email,
+        meet: req.body.meet,
+        message: req.body.message,
+        mailing: req.body.mailing
+    };
+
+    submissions.push(submission);
+    console.log(submissions);
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
 });
 
 app.listen(PORT, () => {
