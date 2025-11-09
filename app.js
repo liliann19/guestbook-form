009f872b0ab60f08454ce2b47385ada2a6c4d76e
Lilian <210286008+liliann19@users.noreply.github.com>
2025-11-09 07:03:53 -0800

    added EJS template, rerouted pages, added resume

--- app.js
+++ app.js
@@ -6,21 +6,34 @@ const PORT = 3002;
 
 const submissions = [];
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true}));
 
+// Resume page
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    //res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
 });
 
+// Contact/guestbook form
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+// Confirmation page
 app.get('/confirm', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+    res.render('confirmation');
 });
 
+// Admin page
 app.get('/admin', (req, res) => {
-    res.send(submissions);
-})
+    //res.send(submissions);
+    res.render('admin', { submissions });
+});
 
 app.post('/submit-request', (req, res) => {
     const submission = {
@@ -34,13 +47,15 @@ app.post('/submit-request', (req, res) => {
         other: req.body.other,
         message: req.body.message,
         mailing: req.body.mailing,
-        format: req.body.format
+        format: req.body.format,
+        timestamp: new Date().toLocaleString()
     };
 
     submissions.push(submission);
     console.log(submissions);
 
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+    res.render('confirmation', { submission });
 });
 
 app.listen(PORT, () => {
